3485e6381ed1adeba6818a6aa586835e09a18400
William Cheng <>
2024-04-24 11:09:56 -0500

    Icon Hover Hook Implemented, home page z-index updated

--- src/components/loaderIconSmall.tsx
+++ src/components/loaderIconSmall.tsx
@@ -9,7 +9,7 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
     <defs>
       <style>
         {
-          ".cls-s{fill:none;stroke:#34412c;stroke-miterlimit:10;stroke-width:2px}"
+          ".cls-s{fill:none;stroke:#34412c;stroke-miterlimit:10;stroke-width:2px;}"
         }
       </style>
     </defs>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,25 @@ import './Home.css';
 import HomeSubPage from '../components/HomeSubPage';
 import { LoaderIconSmall } from '../components/loaderIconSmall';
 import FlowerIMG from "../assets/Blue_Flower.png";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { DrawSVGPlugin } from 'gsap-trial/all';
 
 
 export default function Home(){
+    gsap.registerPlugin(useGSAP, DrawSVGPlugin);
+    var tl = gsap.timeline({paused: true});
+    tl.to(".cls-s", {duration:.8,drawSVG: false, stagger:0.1});
+    
+    const HomeIconIn = () => {
+        console.log("home icon in");
+        tl.play(0);
+    }
+
+    const HomeIconOut = () => {
+        console.log("home icon out");
+    }
+
 
     return(
         <div className='home-page'>
@@ -12,7 +28,7 @@ export default function Home(){
                 <img src={FlowerIMG} alt="" />
             </div>
             <div className='top-row'>
-                <div className='icon-container'>
+                <div className='icon-container' onMouseEnter={HomeIconIn} onMouseLeave={HomeIconOut}>
                     <LoaderIconSmall/>
                 </div>
             </div>
@@ -20,8 +36,6 @@ export default function Home(){
                 <h1 className='bloom'>BLOOM</h1>
             </div>
             <HomeSubPage/>
-
-            
         </div>
     )
 }
