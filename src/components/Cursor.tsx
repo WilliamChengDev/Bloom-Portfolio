e614a9f8b627eb952d1fd12eea838fd34938bc14
William Cheng <>
2024-06-29 23:18:23 -0500

    fixed mouse pointer centering bug, working on reactive cursor animations when hovering over certain items.

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -1,15 +1,23 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import "./Cursor.css"
 import { gsap } from "gsap-trial"
 import { useGSAP } from "@gsap/react"
 
 export default function Cursor(props: {mouseX: number, mouseY:number}) {
+        const[scale, setScale] = useState(.1);
         gsap.registerPlugin(useGSAP);
 
         useEffect(() => {
-                const yPosition = props.mouseY - window.innerHeight*0.05;
-                const xPosition = props.mouseX - window.innerWidth*0.05;
+                const yPosition = props.mouseY - screen.height*scale/2;
+                const xPosition = props.mouseX - screen.width*scale/2 + screen.width*.2*scale;
                 gsap.to(".cursor", {top:yPosition, left:xPosition})
+                if(props.mouseY <= screen.height*0.08){
+                        setScale(0.015);
+                        gsap.to(".cursor", {width:"1.5vw", backgroundColor:"var(--font-color)"})
+                }else{
+                        setScale(.1);
+                        gsap.to(".cursor", {width:"10vw", background:"rgba(255, 255, 255, 0.2)"})
+                }
         }, [props.mouseX, props.mouseY])
 
         return (
